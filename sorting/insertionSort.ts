34fbf5e868c977817c5ae12c154259fbce75d383
Elliott <elliottjprice@outlook.com>
2025-07-24 11:33:01 +1000

    insertionSort

---
+++ sorting/insertionSort.ts
@@ -0,0 +1,25 @@
+/**
+ * Insertion sort (Elliott)
+ * O(n^2)
+ */
+
+function insertionSort(l: number[]) {
+    for (let unsortedIndex = 1; unsortedIndex < l.length; unsortedIndex++) {
+        // sortIndex represents the end of the sorted part of the array
+        // at every step, we take one element from the unsorted part, and move it to the sorted part
+        let unsortedElement = l[unsortedIndex];
+        let checkedIndex = unsortedIndex-1;
+        while (checkedIndex >= 0 && unsortedElement < l[checkedIndex]) {
+            // shift it backwards
+            l[checkedIndex + 1] = l[checkedIndex];
+            checkedIndex--;
+        }
+
+        l[checkedIndex+1] = unsortedElement;
+    }
+}
+
+
+let v = [2,3,5,7,23,1,342,1];
+insertionSort(v)
+console.log(v);
