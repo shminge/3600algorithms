2adfb3b9655ad0be78458ba90249e37e51f9476e
Matthew Hackett <195298367+matthew-hackett@users.noreply.github.com>
2025-07-24 11:25:36 +1000

    Bubble sort

---
+++ sorting/bubbleSort.ts
@@ -0,0 +1,10 @@
+// Bubble sort in-place
+function bubbleSort(l: number[]) {
+    for (let i = 0; i < l.length - 1; i++) {
+        for (let j = 0; j < l.length - i - 1; j++) {
+            if (l[j] > l[j + 1]) {
+                [l[j], l[j + 1]] = [l[j + 1], l[j]];
+            }
+        }
+    }
+}
